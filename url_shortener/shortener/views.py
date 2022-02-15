432fe6b8d2301167e0c04a2423f377cb39e267f4
NikhilGiri29 <nikhu.irig29@gmail.com>
2022-02-16 02:53:55 +0530

    Basic Frontend added + User Input Setup

--- url_shortener/shortener/views.py
+++ url_shortener/shortener/views.py
@@ -3,4 +3,18 @@ from django.shortcuts import render, redirect
 # Create your views here.
 
 def index(request):
-    return render(request,'index.html')
+    link = ""
+    show = False
+    if request.method == "POST":
+        
+        link = request.POST['link']
+        if len(str(link)) !=0:
+            show = True
+            return render(request,'index.html',{'link': link, 'show':show})
+        else:
+            show = False
+
+
+
+    return render(request,'index.html',{'link': link, 'show':show})
+
